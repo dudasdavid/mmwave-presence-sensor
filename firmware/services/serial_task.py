9b3fa924a8fd9c7d0cb47d4a945da57c515007e9
David Dudas <david.dudas@outlook.com>
2026-01-05 10:44:31 +0100

    serial task to read data from mmWave radar

--- firmware/services/serial_task.py
+++ firmware/services/serial_task.py
@@ -1,13 +1,33 @@
 import uasyncio as asyncio
 from machine import UART, Pin
 from logger import Logger
+from drivers import ld2410 as ld2410_driver
+
+# ---- Global variables ----
+import shared_variables as var
 
 async def serial_task(period = 1.0):
     #Init
     log = Logger("uart", debug_enabled=True)
+    
+    uart = UART(
+        0,
+        baudrate=256000,
+        tx=Pin(0),
+        rx=Pin(1)
+    )
+    
+    ld2410 = ld2410_driver.LD2410(uart)
+    #ld2410.set_basic_config(8,8,presence_timeout=5)
+    #ld2410.set_resolution(75)
 
     #Run
     while True:
         log.debug("Task is running")
+        
+        ld2410.read()
+        
+        log.info("mmWave detection:", ld2410.detected)
+        var.mm_wave_detected = ld2410.detected
         
         await asyncio.sleep(period)
